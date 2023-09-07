488557d0db0238c33723bcdfe9be5ec80cc1d98d
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-07 16:07:48 -0500

    added operators and comment

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let muffins = 12;
+let muffins = 24;
 
 document.getElementById("test-1").innerHTML = "Do you know the muffin man?";
+document.getElementById("test-2").innerHTML = "Yes, I know the muffin man!";
+document.getElementById("test-3").innerHTML = (24 - 14) / 2; //this is how many muffins are left for two people to share
 
 console.log(muffins);
