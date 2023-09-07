07fefa7b05e19d15640a4869b91689e3e88d47ea
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-07 13:40:44 -0500

    test variables

---
+++ main.js
@@ -0,0 +1,14 @@
+// var muffins = `12`;
+
+// muffins = muffins - 3;
+// muffins = muffins - 3;
+
+// var name = "Muffin Man";
+
+// const muffins = 12;
+
+// muffins = 6;
+
+let muffins = 12;
+
+console.log(muffins);
