0dca8db3ca0e5182a371d2eb7ea16f2d9167ade8
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-08 13:58:34 -0500

    added string variables

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 let muffins = 24;
 
-document.getElementById("test-1").innerHTML = "Do you know the muffin man?";
-document.getElementById("test-2").innerHTML = "Yes, I know the muffin man!";
+console.log(muffins);
+
 document.getElementById("test-3").innerHTML =
   (24 - 14) / 2 + " muffins are left"; //this is how many muffins are left for two people to share
-
-console.log(muffins);
 
 {
   const a = 6; //this is a const variable within a block
@@ -14,3 +12,8 @@ console.log(muffins);
 {
   const a = 12; //this is another const variable within a different block. the const can change here!
 }
+
+let text1 = "Do you know the muffin man? "; //this is a string variable
+let text2 = "Yes, I know the muffin man! "; //this is also a string variable
+let text3 = text1 + text2; //the two string variables are added together within this third variable!
+document.getElementById("muffin").innerHTML = text3;
