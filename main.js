e95f8ef203c57e874ceb281ea60c6470d1eb9379
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-08 11:10:39 -0500

    added block variables

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ document.getElementById("test-3").innerHTML =
   (24 - 14) / 2 + " muffins are left"; //this is how many muffins are left for two people to share
 
 console.log(muffins);
+
+{
+  const a = 6; //this is a const variable within a block
+  const z = 12; //this is also a const variable within a block
+}
