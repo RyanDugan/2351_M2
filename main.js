f1a4e25ac0fd78b681223fc976feac0a5afc8e7a
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-08 18:23:05 -0500

    corrected spelling

--- main.js
+++ main.js
@@ -27,4 +27,4 @@ const muffintype = {
 document.getElementById("muffintype").innerHTML =
   muffintype.flavor + " with " + muffintype.topping;
 
-document.getElementById("testglobal").innerHTML = muffins; //this proves the global variable is funcitoning properly!
+document.getElementById("testglobal").innerHTML = muffins; //this proves the global variable is functioning properly!
