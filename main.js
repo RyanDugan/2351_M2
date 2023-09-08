185afe7130ed80e3650a55d7ac59760b675edfcb
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-08 10:59:32 -0500

    adjusted spacing txt

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ let muffins = 24;
 document.getElementById("test-1").innerHTML = "Do you know the muffin man?";
 document.getElementById("test-2").innerHTML = "Yes, I know the muffin man!";
 document.getElementById("test-3").innerHTML =
-  (24 - 14) / 2 + " " + "muffins are left"; //this is how many muffins are left for two people to share
+  (24 - 14) / 2 + " muffins are left"; //this is how many muffins are left for two people to share
 
 console.log(muffins);
